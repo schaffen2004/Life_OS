7bf46e339539cab886c81ac718d1e4fb6f991aa0
Schaffen <laphv494@gmail.com>
2024-10-31 14:40:25 +0700

    Upload data

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def run():
     st.title('My App')
-    df = pd.read_csv('.\data\habit.csv')
+    df = pd.read_csv('data\habit.csv')
     st.write(df)
 
 if __name__ == '__main__':
